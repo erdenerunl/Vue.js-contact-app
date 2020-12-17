ffdf7e6b3b39d5b088a39838d6eed0a45fa3228c
erdenerunl <erdenerunale@gmail.com>
2020-12-17 16:14:23 +0300

    last commit

--- src/App.vue
+++ src/App.vue
@@ -3,36 +3,37 @@
     <header>Contact Card App</header>
     <div class="container">
       <div class="contact--card--app">
-        <contact-section v-for="(contact, index) in 10" :key="index" :contactData="contactData" />
+        <contact-section
+          v-for="(contact, index) in 10"
+          :key="index"
+          :contactData="contactData"
+        />
       </div>
     </div>
   </div>
 </template>
 
 <script>
-import ContactSection from './components/ContactSection.vue'
+import ContactSection from "./components/ContactSection.vue";
 
 export default {
   components: {
-    ContactSection
+    ContactSection,
   },
-  data(){
+  data() {
     return {
-      contactData : {
-        id : 1,
-        name : 'Gökhan Kandemir',
-        title : 'Senior Software Engineer at PureSOL',
-        url : '#',
-        linkCounter : 150,
+      contactData: {
+        id: 1,
+        name: "Gökhan Kandemir",
+        title: "Senior Software Engineer at PureSOL",
+        url: "#",
+        linkCounter: 150,
+        photoLink: 'https://media-exp1.licdn.com/dms/image/C5603AQFjWy_2_wMdFw/profile-displayphoto-shrink_200_200/0?e=1609372800&v=beta&t=7NQhzZFjrnV0ASU_LhPEWTEm12ZtqM6mrVv9XNUZFrE'
       },
-      
-    }
+    };
   },
-  
-
-}
+};
 </script>
 
 <style>
-
 </style>
--- src/components/ContactSection.vue
+++ src/components/ContactSection.vue
@@ -4,7 +4,7 @@
       <div class="invitation--header">
         <img
           class="contact--image mt-20"
-          src="https://media-exp1.licdn.com/dms/image/C5603AQFjWy_2_wMdFw/profile-displayphoto-shrink_200_200/0?e=1609372800&v=beta&t=7NQhzZFjrnV0ASU_LhPEWTEm12ZtqM6mrVv9XNUZFrE"
+          :src="contactData.photoLink"
         />
       </div>
       <div class="contact--info--container">
@@ -24,6 +24,7 @@
 
 <script>
 export default {
+    
     props : ['contactData']
 };
 </script>
